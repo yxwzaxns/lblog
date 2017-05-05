5841d2ef438a016916a87975b4558bcd10a6ba78
aong <yxwzaxns@gmail.com>
2017-05-06 00:24:07 +0800

    2333

--- deploy.php
+++ deploy.php
@@ -7,20 +7,32 @@ require 'recipe/laravel.php';
 set('ssh_type', 'native');
 set('ssh_multiplexing', true);
 
-set('repository', 'git@domain.com:username/repository.git');
+set('repository', 'git@github.com:yxwzaxns/lblog.git');
 
-add('shared_files', []);
+add('shared_files', [
+    '.env',
+]);
 add('shared_dirs', []);
 
-add('writable_dirs', []);
+add('writable_dirs', [
+    'bootstrap/cache',
+    'storage',
+    'storage/app',
+    'storage/app/public',
+    'storage/framework',
+    'storage/framework/cache',
+    'storage/framework/sessions',
+    'storage/framework/views',
+    'storage/logs',
+]);
 
 // Servers
 
-server('production', 'domain.com')
-    ->user('username')
+server('production', '120.24.65.9')
+    ->user('web')
     ->identityFile()
-    ->set('deploy_path', '/var/www/domain.com')
-    ->pty(true);
+    ->set('deploy_path', '/home/web/www');
+//    ->pty(true);
 
 
 // Tasks
@@ -29,7 +41,7 @@ desc('Restart PHP-FPM service');
 task('php-fpm:restart', function () {
     // The user must have rights for restart service
     // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
-    run('sudo systemctl restart php-fpm.service');
+    run('sudo service php7.0-fpm restart');
 });
 after('deploy:symlink', 'php-fpm:restart');
 
